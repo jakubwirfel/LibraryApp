c1fe8ff3b6fc586ca8cb76fa1e4747ec871b829b
jakubwirfel <jakubwirfel@gmail.com>
2020-01-11 19:54:55 +0100

    init GIT repo and basic directory

---
+++ public/public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Library APP</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="./public/css/main.css">
+    </head>
+    <body>
+        <h1 class="dupa">Dupa</h1>
+    </body>
+</html>
