4e8eda95f9e60b7e01115fd4adfea0d5614578ff
SeNzO <jakubwirfel@gmail.com>
2020-03-16 20:48:22 +0100

    Add new DB library.sql

--- library.sql
+++ library.sql
@@ -2,10 +2,10 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Czas generowania: 28 Sty 2020, 12:36
--- Wersja serwera: 10.4.10-MariaDB
--- Wersja PHP: 7.3.12
+-- Host: localhost
+-- Czas generowania: 16 Mar 2020, 20:44
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -53,6 +53,13 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `user_name` varchar(60) NOT NULL,
   `user_email` varchar(60) NOT NULL,
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `phone_num` varchar(11) NOT NULL,
+  `post_code` varchar(6) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `street` varchar(50) NOT NULL,
+  `house_num` varchar(7) NOT NULL,
   `user_password` varchar(60) NOT NULL,
   `user_pwd_change` tinyint(1) DEFAULT NULL,
   `user_pwd_change_date` date NOT NULL,
@@ -63,11 +70,11 @@ CREATE TABLE `users` (
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `user_password`, `user_pwd_change`, `user_pwd_change_date`, `group`) VALUES
-(1, 'test', 'test@test.pl', '$2y$10$UF0EMjMbHD/9uCgc6Rst3evTXzY2A91V0ZzeZLnvHZBO0JNRIylny', 0, '2020-01-26', 3),
-(2, 'user', 'user@user', '$2y$10$vQnIY9p2LGjg2oCAt7pVNeNyQg9rkOG88k0zx7XdIr41jmI.6u92a', 0, '2020-01-26', 1),
-(3, 'moderator', 'moderator@moderator', '$2y$10$idvxh/kz8tgerkn8wyFsIej5JdBVjSnOCWHSqUCxGVIO75svAn/Bu', 0, '2020-01-26', 2),
-(4, 'admin', 'admin@admin', '$2y$10$7qurgQjRDa8ntPK45LzOVeZy68nYfiCw8Co6H4B9RZdkzmOIYeeQu', 0, '2020-01-26', 3);
+INSERT INTO `users` (`user_id`, `user_name`, `user_email`, `first_name`, `last_name`, `phone_num`, `post_code`, `city`, `street`, `house_num`, `user_password`, `user_pwd_change`, `user_pwd_change_date`, `group`) VALUES
+(2, 'user', 'user@user', 'Usernamek', 'Userlast', '111-111-111', '11-111', 'Userkowo', 'Userkowa', '1/1', '$2y$10$vQnIY9p2LGjg2oCAt7pVNeNyQg9rkOG88k0zx7XdIr41jmI.6u92a', 0, '2020-01-26', 1),
+(3, 'moderator', 'moderator@moderator', 'Modernamek', 'Modernlastnek', '222-222-222', '22-222', 'Modernakowo', 'Moderowa', '2/2', '$2y$10$idvxh/kz8tgerkn8wyFsIej5JdBVjSnOCWHSqUCxGVIO75svAn/Bu', 0, '2020-01-26', 2),
+(4, 'admin', 'admin@admin', 'Adminamek', 'Admineczek', '333-333-333', '33-333', 'Adminkowo', 'Adminkowa', '3/3', '$2y$10$7qurgQjRDa8ntPK45LzOVeZy68nYfiCw8Co6H4B9RZdkzmOIYeeQu', 0, '2020-01-26', 3),
+(10, 'myszka', 'agasshi@onet.eu', 'Myszka', 'Szczurkowa', '888-888-888', '88-888', 'Gryzonice', 'Serowa', '666', '$2y$10$eu1.yVCPy6VijFss/2MAD.mrkguFiX1PdaiOelclGKKNQca9hcM1O', 0, '2020-03-07', 1);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -100,7 +107,7 @@ ALTER TABLE `groups`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ograniczenia dla zrzutów tabel
