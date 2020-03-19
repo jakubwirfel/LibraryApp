62e4314e97ecfc281908d8cacf16653f62a5a72f
jakubwirfel <jakubwirfel@gmail.com>
2020-03-19 21:07:59 +0100

    Add update user to DB. Working, couple fixes to do

--- src/classes/user_services.class.php
+++ src/classes/user_services.class.php
@@ -33,5 +33,27 @@ class UserServices extends Validators {
             array_push($errors, $e->getMessage());
         }
     }
+    public function modifyUser($userId, $userName, $firstName, $lastName,$userEmail, $phoneNum, $postCode, $city, $street, $houseNum, $userPwdChange, $group) {
+        try {
+            global $errors;
+            $sql = "UPDATE users INNER JOIN groups on users.group = groups.id SET user_name=:userName, first_name=:firstName, last_name=:lastName, user_email=:userEmail, phone_num=:phoneNum, post_code=:postCode, city=:city, street=:street, house_num=:houseNum, user_pwd_change=:userPwdChange, users.group=:group WHERE user_id=:userId";
+            $query = $this -> db -> prepare($sql);
+            $query -> bindParam(":userName", $userName);
+            $query -> bindParam(":userId", $userId);
+            $query -> bindParam(":firstName", $firstName);
+            $query -> bindParam(":lastName", $lastName);
+            $query -> bindParam(":userEmail", $userEmail);
+            $query -> bindParam(":phoneNum", $phoneNum);
+            $query -> bindParam(":postCode", $postCode);
+            $query -> bindParam(":city", $city);
+            $query -> bindParam(":street", $street);
+            $query -> bindParam(":houseNum", $houseNum);
+            $query -> bindParam(":userPwdChange", $userPwdChange);
+            $query -> bindParam(":group", $group);
+            $query -> execute();
+        } catch (PDOException $e) {
+            array_push($errors, $e->getMessage());
+        }
+    }
 }
 ?>
--- user_display.php
+++ user_display.php
@@ -1,6 +1,6 @@
 <?php
     include_once './src/include/start.inc.php';
-
+    $UserServices = new UserServices($database);
     if (!$user->is_logged_in()) {
         $user->redirect('login.php');
     }
@@ -24,7 +24,7 @@
     }
 
     try {
-        $userId = $_GET['user'];
+        $userId = 10;
         $sql2 = "SELECT * FROM users INNER JOIN groups on users.group = groups.id WHERE user_id LIKE :user_id LIMIT 1";
         $query2 = $database -> prepare($sql2);
         $query2 -> bindParam(":user_id", $userId);
@@ -33,6 +33,25 @@
         array_push($errors, $e->getMessage());
     }
 
+    if(isset($_POST['update']) && $_POST['update'] == 'Update'){
+        try {
+            $userId = $_POST['userId'];
+            $userName = $_POST['userName'];
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $userEmail = $_POST['email'];
+            $phoneNum = $_POST['phone'];
+            $postCode = $_POST['postCode'];
+            $city = $_POST['city'];
+            $street = $_POST['street'];
+            $houseNum = $_POST['houseNum'];
+            $userPwdChange = $_POST['userPwdChange'];
+            $group = $_POST['group'];
+            $UserServices -> modifyUser($userId, $userName, $firstName, $lastName, $userEmail, $phoneNum, $postCode, $city, $street, $houseNum, $userPwdChange, $group);
+        } catch (PDOException $e) {
+            array_push($errors, $e->getMessage());
+        }
+    }
     if(($returned['group'] != "1") && ($returned['permissions'] != "user = 1")) :
         while($usersRow = $query2->fetch(PDO::FETCH_ASSOC)) {
 ?>
@@ -56,23 +75,24 @@
         <h1 class="navbar-brand">User <u><?php echo $usersRow['user_name'] . "/" . $usersRow['user_id'] ?> </u>display and modification</h1>
     </nav>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" class="p-5">
+    <input type="hidden" name="userId"value="<?php echo $usersRow['user_id']?>">
     <div class="btn btn-primary mt-5" id="ToogleFieldset"><i class="fas fa-pen"></i> Edit</div>
     <hr>
     <fieldset id="Fieldset" disabled>
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">First name</label>
+                    <label for="inputFirstName" class="col-sm-auto col-form-label">First name</label>
                     <div class="col-sm">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['first_name']?>">
+                        <input type="text" name="firstName" class="form-control" id="inputFirstName" value="<?php echo $usersRow['first_name']?>">
                     </div>
                 </div>
             </div>
             <div class="col ">
             <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Last name</label>
+                    <label for="inputLastName" class="col-sm-auto col-form-label">Last name</label>
                     <div class="col-sm">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['last_name']?>">
+                        <input type="text" name="lastName" class="form-control" id="inputLastName" value="<?php echo $usersRow['last_name']?>">
                     </div>
                 </div>
             </div>
@@ -80,17 +100,17 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Email</label>
+                    <label for="inputEmail" class="col-sm-auto col-form-label">Email</label>
                     <div class="col-sm">
-                        <input type="email" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['user_email']?>">
+                        <input type="email" name = "email" class="form-control" id="inputEmail" value="<?php echo $usersRow['user_email']?>">
                     </div>
                 </div>
             </div>
             <div class="col ">
             <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Phone num.</label>
+                    <label for="inputPhone" class="col-sm-auto col-form-label">Phone num.</label>
                     <div class="col-sm">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['phone_num']?>">
+                        <input type="text" name ="phone" class="form-control" id="inputPhone" value="<?php echo $usersRow['phone_num']?>">
                     </div>
                 </div>
             </div>
@@ -98,12 +118,12 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Post. code / City</label>
+                    <label for="inputPostCode" class="col-sm-auto col-form-label">Post. code / City</label>
                     <div class="col-sm-3">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['post_code']?>">
+                        <input type="text" name="postCode" class="form-control" id="inputPostCode" value="<?php echo $usersRow['post_code']?>">
                     </div>
                     <div class="col-sm">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['city']?>">
+                        <input type="text" name="city" class="form-control"  value="<?php echo $usersRow['city']?>">
                     </div>
                 </div>
             </div>
@@ -111,12 +131,12 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Street / House. num</label>
+                    <label for="inputStreet" class="col-sm-auto col-form-label">Street / House. num</label>
                     <div class="col-sm">
-                        <input type="text" class="form-control"  id="inputEmail3" placeholder="<?php echo $usersRow['street']?>">
+                        <input type="text" name="street" class="form-control"  id="inputStreet" value="<?php echo $usersRow['street']?>">
                     </div>
                     <div class="col-sm-2">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['house_num']?>">
+                        <input type="text" name="houseNum"class="form-control" value="<?php echo $usersRow['house_num']?>">
                     </div>
                 </div>
             </div>
@@ -125,9 +145,10 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">User password change? </label>
+                    <label for="inputPwd" class="col-sm-auto col-form-label">User password change? </label>
                     <div class="col-sm-1">
-                        <input type="radio"<?php if($usersRow['user_pwd_change'] == 1){echo "checked";}?> class="form-control" id="inputEmail3">
+                        <input type="hidden" value="0" name="userPwdChange">
+                        <input type="radio"<?php if($usersRow['user_pwd_change'] == 1){echo "checked";}?> name="userPwdChange" class="form-control" id="inputPwd" value="1">
                     </div>
                 </div>
             </div>
@@ -135,9 +156,9 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">Last password change </label>
+                    <label for="inputPwdDate" class="col-sm-auto col-form-label">Last password change </label>
                     <div class="col-sm">
-                        <input disabled type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['user_pwd_change_date']?>">
+                        <input disabled type="text" class="form-control" id="inputPwdDate" placeholder="<?php echo $usersRow['user_pwd_change_date']?>">
                     </div>
                 </div>
             </div>
@@ -145,9 +166,9 @@
         <div class="form-group row">
             <div class="col">
                 <div class="row">
-                    <label for="inputEmail3" class="col-sm-auto col-form-label">User group</label>
+                    <label for="inputGroup" class="col-sm-auto col-form-label">User group</label>
                     <div class="col-sm-2">
-                        <input type="text" class="form-control" id="inputEmail3" placeholder="<?php echo $usersRow['group']?>">
+                        <input type="text" name="group" class="form-control" id="inputGroup" value="<?php echo $usersRow['group']?>">
                     </div>
                     <div class="col-sm">
                         <input disabled type="text" class="form-control" id="inputEmail3" placeholder="<?php switch($usersRow['group']){case 1: echo "User"; break;
@@ -157,9 +178,26 @@
                 </div>
             </div>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group row">
+            <div class="col">
+                <div class="row">
+                    <label for="inputUsername" class="col-sm-auto col-form-label">Username</label>
+                    <div class="col-sm">
+                        <input type="text" name="userName" class="form-control" id="inputUsername" value="<?php echo $usersRow['user_name']?>">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button type="submit" name="update" value="Update" class="btn btn-primary">Submit</button>
     </fieldset>
     </form>
+    <?php if (!$errors < 1): ?>
+            <div class="errors_box_default">
+                <?php foreach ($errors as $error): ?>
+                    <p><?= $error ?></p>
+                <?php endforeach ?>
+            </div>
+    <?php endif ?>
     <script>
         var toogleFieldset = document.getElementById("ToogleFieldset");
         var fieldset = document.getElementById("Fieldset");
